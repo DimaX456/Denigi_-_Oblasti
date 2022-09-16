46aff2654ae65a440bbe17975f45ce254fb4caaa
DimaX456 <106971904+DimaX456@users.noreply.github.com>
2022-09-17 00:39:11 +0300

    Add files via upload

---
+++ Denigi_Dollar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Denigi_Dollar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.Write("Введите количество долларов, для того, чтобы перевести доллар в рубли: ");
+                var dollarInput = Console.ReadLine();
+                int.TryParse(dollarInput, out int dollarOutput);
+                var result = dollarOutput * 60;
+                var proc = 0.37;
+                Console.WriteLine($"Вывод: {result:#,###} рублей.");
+                if 
+                (dollarOutput < 500) 
+                Console.WriteLine($"Было взято {8} рублей, вывод: {(result - 8):#,###} рублей.");
+                else 
+                Console.WriteLine($"Было взято {proc}%, вывод: {(result - (result * proc / 100)):#,###} рублей.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Primoygolinik)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primoygolinik_
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину для прямоугольника: ");
+            var WidthOutput = Console.ReadLine();
+            Console.Write("Введите высоту для прямоугольника: ");
+            var HeightOutput = Console.ReadLine();
+            if (int.TryParse(WidthOutput, out int Width) && int.TryParse(HeightOutput, out int Height))
+            {
+                var x = (Console.WindowWidth - Width) / 2;
+                var y = (Console.WindowHeight - Height) / 2;
+                Console.SetCursorPosition(x, y);
+
+                for (int i = 0; i <= Height; i++)
+                {
+                    for (int j = 0; j < Width; j++)
+                    {
+                        Console.Write("#");
+                    }
+                    Console.SetCursorPosition(x, y++);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
